8eb2ce75aab40df08c49cf4867ac7eaadc54900c
Kyng Cytro <johndibashi2001@gamil.com>
2024-06-16 10:48:13 +0100

    feat: add gopher nvim and update silicon config

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -17,4 +17,14 @@ return {
       },
     },
   },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd([[silent! GoInstallDeps]])
+    end,
+  },
 }
--- lua/plugins/silicon.lua
+++ lua/plugins/silicon.lua
@@ -3,7 +3,7 @@ return {
   lazy = true,
   cmd = "Silicon",
   config = function()
-    require("silicon").setup({
+    require("nvim-silicon").setup({
       font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
       window_title = function()
         return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
