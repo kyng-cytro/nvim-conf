586845ef3adc912a94031af233e8221d9835318c
kyng-cytro <johndibashi2001@gmail.com>
2025-05-07 03:45:32 +0100

    chore: updates

--- lua/plugins/super-maven.lua
+++ lua/plugins/super-maven.lua
@@ -1,15 +1,13 @@
 return {
   "supermaven-inc/supermaven-nvim",
-  config = function()
-    require("supermaven-nvim").setup({
-      keymaps = {
-        accept_suggestion = "<C-]>",
-        clear_suggestion = "<C-[>",
-      },
-      color = {
-        suggestion_color = "#45475A",
-        cterm = 244,
-      },
-    })
-  end,
+  opts = {
+    keymaps = {
+      accept_suggestion = "<C-]>",
+      clear_suggestion = "<C-[>",
+    },
+    color = {
+      suggestion_color = "#45475A",
+      cterm = 244,
+    },
+  },
 }
